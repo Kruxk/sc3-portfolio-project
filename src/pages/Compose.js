2f60216950a7ac74d6df12d589efea4ab8328c92
reinko <reinko.brink@gmail.com>
2020-06-17 20:55:37 +0200

    Revert "Pages"

--- src/pages/Compose.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Compose() {
-  return <div>COMPOSETEST</div>;
-}
-
-export default Compose;
--- src/pages/Texture.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Texture() {
-  return <div>TEXTURETEST</div>;
-}
-
-export default Texture;
