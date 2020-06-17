31c8214f49acf8b18c0c330ca7cbb1640dd5dc82
Kruxk <reinko.brink@gmail.com>
2020-06-17 16:08:43 +0200

    basic setup for pages

---
+++ src/pages/Compose.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Compose() {
+  return <div>COMPOSETEST</div>;
+}
+
+export default Compose;
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div>HOMETEST</div>;
+}
+
+export default Home;
