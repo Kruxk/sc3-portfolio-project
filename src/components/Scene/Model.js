f0600473f99508a989f2dade3ba281c20cbe1b58
Kruxk <reinko.brink@gmail.com>
2020-06-18 09:39:55 +0200

    added redux-thunk

--- src/components/Scene/Model.js
+++ src/components/Scene/Model.js
@@ -4,9 +4,10 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 function Model() {
   const [model, setModel] = useState();
   console.log(model);
+  if (model) model.scene.visible = true;
   useEffect(() => {
     new GLTFLoader().load(
-      "/TOOTHPASTE_Placeholder_TrueScale_v1.gltf",
+      "/models/TANDPASTA-PRODENT-ROUNDED-001_v1.glb",
       setModel
     );
   }, []);
